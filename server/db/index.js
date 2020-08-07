3fd90196193054767014a80ecd1d4edc45e0c1e5
Daniel <ogbuikedaniel@gmail.com>
2020-08-07 13:18:11 +0100

    Inital Commit

---
+++ server/db/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/cinema",{useNewUrlParser:true,useUnifiedTopology: true,})
+
+const db = mongoose.connection
+
+module.exports = db
